6225c5496c7f1f6a731511a711b39c2a567b4759
Manoelito Holanda <manoelito_holanda@hotmail.com>
2025-07-13 21:02:30 -0300

    add find by nome

--- src/main/java/com/biblioteca/biblioteca_digital/repository/AutorRepository.java
+++ src/main/java/com/biblioteca/biblioteca_digital/repository/AutorRepository.java
@@ -3,5 +3,8 @@ package com.biblioteca.biblioteca_digital.repository;
 import com.biblioteca.biblioteca_digital.model.entity.Autor;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AutorRepository extends JpaRepository<Autor, Long> {}
+import java.util.Optional;
 
+public interface AutorRepository extends JpaRepository<Autor, Long> {
+    Optional<Autor> findByNomeIgnoreCase(String nome);
+}
