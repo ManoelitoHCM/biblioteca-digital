0c2d2fb409c47773a55fabfd9df9bd69cd916af4
Manoelito Holanda <manoelito_holanda@hotmail.com>
2025-07-14 21:25:35 -0300

    migrando diretorio

--- src/main/java/com/biblioteca/biblioteca_digital/common/util/ISBNValidator.java
+++
@@ -1,4 +0,0 @@
-package com.biblioteca.biblioteca_digital.common.util;
-
-public class ISBNValidator {
-}
---
+++ src/main/java/com/biblioteca/biblioteca_digital/validation/ISBNValidator.java
@@ -0,0 +1,4 @@
+package com.biblioteca.biblioteca_digital.validation;
+
+public class ISBNValidator {
+}
