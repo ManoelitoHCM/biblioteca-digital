70601682d3831cada04afd316aea7437a4f6fcd8
Manoelito Holanda <manoelito_holanda@hotmail.com>
2025-07-14 21:03:02 -0300

    remoção das classes de exceção por conflito com swagger

--- src/main/java/com/biblioteca/biblioteca_digital/common/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.biblioteca.biblioteca_digital.common.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String mensagem) {
-        super(mensagem);
-    }
-}
