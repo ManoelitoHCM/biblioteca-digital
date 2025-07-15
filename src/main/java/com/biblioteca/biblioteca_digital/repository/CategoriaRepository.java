b1823fdd914eeddfdce4195ab6c14e8b909eb95b
Manoelito Holanda <manoelito_holanda@hotmail.com>
2025-07-14 21:03:11 -0300

    find by nome

--- src/main/java/com/biblioteca/biblioteca_digital/repository/CategoriaRepository.java
+++ src/main/java/com/biblioteca/biblioteca_digital/repository/CategoriaRepository.java
@@ -3,4 +3,8 @@ package com.biblioteca.biblioteca_digital.repository;
 import com.biblioteca.biblioteca_digital.model.entity.Categoria;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CategoriaRepository extends JpaRepository<Categoria, Long> {}
+import java.util.Optional;
+
+public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+    Optional<Categoria> findByNomeIgnoreCase(String nome);
+}
